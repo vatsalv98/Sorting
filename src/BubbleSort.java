6f77806cda6a427ea118de9294a886a099e0e97f
vatsal <vatsalv98@gmail.com>
2019-01-31 09:45:06 +0530

    merge branch commit for sorting

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -28,7 +28,7 @@ public class BubbleSort {
     public static void main(String args[])
     {
         BubbleSort ob = new BubbleSort();
-        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        int arr[] = {64, 34, 25, 12, 22, 11, 90,99};
         ob.bubbleSort(arr);
         System.out.println("Sorted array");
         ob.printArray(arr);
